0ba4e82def1eb0042276665de85c5b69bd3e3552
Smit150907 <smitpkako@gmail.com>
2024-08-30 13:41:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,18 +4,18 @@ from pyrogram import Client, idle
 from pyromod import listen  # type: ignore
 from pyrogram.errors import ApiIdInvalid, ApiIdPublishedFlood, AccessTokenInvalid
 
+# Configure logging
+logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - \033[32m%(pathname)s: \033[31m\033[1m%(message)s \033[0m")
 
-logging.basicConfig(level=logging.INFO, encoding="utf-8", format="%(asctime)s - %(levelname)s - \033[32m%(pathname)s: \033[31m\033[1m%(message)s \033[0m")
-
+# Initialize the Pyrogram client
 app = Client(
     "Session_bot",
     api_id=env.API_ID,
     api_hash=env.API_HASH,
     bot_token=env.BOT_TOKEN,
     in_memory=True,
-    plugins={'root':'StringSessionBot'},
+    plugins={'root': 'StringSessionBot'},
 )
-
 
 if __name__ == "__main__":
     logging.info("Starting the bot")
